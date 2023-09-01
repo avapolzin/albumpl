2a0573a6d5f7e2cb00ea30994308a0ecdaca3c8e
avapolzin <apolzin@uchicago.edu>
2023-09-01 01:07:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name = "albumpl",
-	version = "0.1",
+	version = "0.2.0",
 	author = "Ava Polzin",
 	author_email = "apolzin@uchicago.edu",
 	description = "Matplotlib color palettes based on album covers.",
